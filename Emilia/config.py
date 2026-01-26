5e40d519a116228b0fbeb26298cd48aa69bdc971
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-01-26 21:49:14 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -7,29 +7,29 @@ def get_user_list(config, key):
         return orjson.loads(json_file.read())[key]
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "79b81186aefef9ca9216c646152ffe45" # API_HASH from my.telegram.org
+    API_ID = 32772113 # API_ID from my.telegram.org
 
     BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Elf_Robot" # BOT_USERNAME
+    BOT_USERNAME = "Gcmanager18_bot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # MongoDB URL from MongoDB Atlas
 
     SUPPORT_CHAT = "SpiralTechDivision" # Support Chat Username
     UPDATE_CHANNEL = "SpiralUpdates" # Update Channel Username
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
+    DEV_USERS = [7028236763] # Dev Users
     TOKEN = "57375" # Bot Token from @BotFather
     CLONE_LIMIT = 50 # Number of clones your bot can make
 
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
     REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-    EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    EVENT_LOGS = --1003877180460 # Event Logs Chat ID
+    OWNER_ID = 7028236763 # Owner ID
  
     TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
-    BOT_NAME = "Emilia" # Bot Name
+    BOT_NAME = "Sohman bot" # Bot Name
     WALL_API = "6950f53" # Wall API from wall.alphacoders.com
     GROQ_API_KEY = "gsk_mm" # GROQ API Key from groq.com
 
