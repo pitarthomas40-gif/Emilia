2a9f51af78c7559d698c5bbae9ebbbbb26cb12df
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-01-27 18:06:59 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -23,8 +23,8 @@ class Config(object):
     TOKEN = "8392569167:AAEC2BmJhoax7eYi-C1hNcTvzPM9z9YMtGY"
     CLONE_LIMIT = 50
 
-    REDIS_URL = None
-    REDIS_PASSWORD = None   # ✅ ab sahi jagah
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
     EVENT_LOGS = -1003877180460
     OWNER_ID = 7028236763
