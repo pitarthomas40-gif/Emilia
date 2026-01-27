da3e7c695d45d31ab8aabc99d08711b9141adbd5
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-01-27 11:43:21 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -6,32 +6,33 @@ def get_user_list(config, key):
     with open("{}/Emilia/{}".format(os.getcwd(), config), "rb") as json_file:
         return orjson.loads(json_file.read())[key]
 
+
 class Config(object):
-    API_HASH = "79b81186aefef9ca9216c646152ffe45" # API_HASH from my.telegram.org
-    API_ID = 32772113 # API_ID from my.telegram.org
+    API_HASH = "79b81186aefef9ca9216c646152ffe45"
+    API_ID = 32772113
 
-    BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Gcmanager18_bot" # BOT_USERNAME
+    BOT_ID = 521
+    BOT_USERNAME = "Gcmanager18_bot"
 
-    MONGO_DB_URL = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-    SUPPORT_CHAT = "rosegroupsohman" # Support Chat Username
-    UPDATE_CHANNEL = "rosebotsohman" # Update Channel Username
-    START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [7028236763] # Dev Users
-    TOKEN = "8392569167:AAEC2BmJhoax7eYi-C1hNcTvzPM9z9YMtGY" # Bot Token from @BotFather
-    CLONE_LIMIT = 50 # Number of clones your bot can make
+    SUPPORT_CHAT = "rosegroupsohman"
+    UPDATE_CHANNEL = "rosebotsohman"
+    START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp"
+    DEV_USERS = [7028236763]
+    TOKEN = "8392569167:AAEC2BmJhoax7eYi-C1hNcTvzPM9z9YMtGY"
+    CLONE_LIMIT = 50
 
     REDIS_URL = None
-REDIS_PASSWORD = None
+    REDIS_PASSWORD = None   # ✅ ab sahi jagah
 
-    EVENT_LOGS = -1003877180460 # Event Logs Chat ID
-    OWNER_ID = 7028236763 # Owner ID
- 
-    TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
-    BOT_NAME = "Sohman bot" # Bot Name
-    WALL_API = "6950f53" # Wall API from wall.alphacoders.com
-    GROQ_API_KEY = "gsk_mm" # GROQ API Key from groq.com
+    EVENT_LOGS = -1003877180460
+    OWNER_ID = 7028236763
+
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    BOT_NAME = "Sohman bot"
+    WALL_API = "6950f53"
+    GROQ_API_KEY = "gsk_mm"
 
 
 class Production(Config):
