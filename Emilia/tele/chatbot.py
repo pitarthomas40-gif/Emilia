d08d4c547385dbd655f6445624679c66a73ff986
pitarthomas40-gif <pitarthomas40@gmail.com>
2026-02-15 14:02:09 +0530

    Update chatbot.py

--- Emilia/tele/chatbot.py
+++ Emilia/tele/chatbot.py
@@ -355,11 +355,12 @@ async def sendResponse(event, response: str):
         finally:
             if os.path.exists(temp_path):
                 os.remove(temp_path)
-    else:
+        else:
         await event.reply(response)
-        
-       async def shutdown_chatbot():
+
+async def shutdown_chatbot(): # ✅ Bilkul left side se shuru karein
     print("Chatbot shutdown successfully")
+
 
 
 PERSONA_DETAILS = """
